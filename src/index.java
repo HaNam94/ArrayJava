93c713436a5b8d5e8e6569a8c7c4c49499d99dbb
HaNam94 <haduynam.94@gmail.com>
2024-04-05 11:37:10 +0700

    Array

---
+++ src/index.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class index {
+    public static void main(String[] args) {
+//        // khai bao mang
+//        int[] array = new int[10];// khoi tao 1 mang int chua toi da 10 phan tu
+//        // khi khởi động như trên mảng sẽ tự động có phần tử mặc định tương úng với kiểu dữ liệu(int: 0; double: 0.0; char: ''; String: null)
+////        array[0] = "Linh"; // Không thể chưa kiêu dữ liệu khác
+//        System.out.println(array[5]); // truy cập phần tử ở vị trí thứ 5
+//        System.out.println(Arrays.toString(array)); // mảng trong Java chỉ
+
+        int[] arr = {1, 2, 3, 32, 45, 98} ;// Cách khai báo 2
+    }
+    public static void getA(int[] arr, int a){
+
+    }
+}
